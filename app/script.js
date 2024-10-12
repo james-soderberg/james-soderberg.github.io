37c34e359bc4a7aa6fb91abf1ed6d54223e5e6ed
jsoderberg <james_soderberg@intuit.com>
2024-10-12 11:50:54 -0600

    better

--- app/script.js
+++ app/script.js
@@ -2,23 +2,38 @@ let numberOfTiles = 10; // Change to adjust grid size
 let winningTile = Math.floor(Math.random() * numberOfTiles * numberOfTiles);
 let tiles = [];
 let gameBoard = document.querySelector("#game-board");
+let gameContainer = document.querySelector("#game-container");
 
 // Dynamically create grid and add click event listeners
 for (let i = 0; i < numberOfTiles * numberOfTiles; i++) {
     let tile = document.createElement("div");
     tile.classList.add("tile");
     tile.id = i.toString(); // Set the id of each tile
+    tile.addEventListener("click", handleClick);
     gameBoard.appendChild(tile);
     tiles.push(tile);
+}
 
-    tile.addEventListener("click", function() {
-        if (i === winningTile) {
-            showWinningScreen();
-        } else {
-            showDistance(i);
-        }
-        this.classList.add("flipped");
-    });
+// Handle click event
+function handleClick() {
+    let clickedTile = this;
+    let clickedTileIndex = tiles.indexOf(clickedTile);
+
+    if (clickedTile.classList.contains("flipped") || clickedTile.classList.contains("animated")) {
+        return;
+    }
+
+    if (clickedTileIndex === winningTile) {
+        showWinningScreen();
+    } else {
+        showDistance(clickedTileIndex);
+    }
+
+    clickedTile.classList.add("animated", "flipping");
+    setTimeout(() => {
+        clickedTile.classList.remove("flipping");
+        clickedTile.classList.add("flipped");
+    }, 500);
 }
 
 // Handle play again button click
@@ -26,7 +41,7 @@ function handlePlayAgainClick() {
     // Reset the state of the game
     winningTile = Math.floor(Math.random() * numberOfTiles * numberOfTiles);
     for (let i = 0; i < numberOfTiles * numberOfTiles; i++) {
-        tiles[i].classList.remove("flipped", "clickable", "winning", "distance");
+        tiles[i].classList.remove("flipped", "animated", "clickable", "winning", "distance");
     }
     hideGameOverScreen();
 }
@@ -41,16 +56,16 @@ function showWinningScreen() {
     }
     let gameOverScreen = document.querySelector("#game-over");
     gameOverScreen.style.display = "block";
-    let playAgainButton = document.querySelector(".play-again-button");
+    let playAgainButton = document.querySelector("#play-again-button");
     playAgainButton.addEventListener("click", handlePlayAgainClick);
 }
 
 // Show the distance between the clicked tile and the winning tile
-function showDistance(clickedTile) {
-    let distance = getDistance(clickedTile, winningTile);
+function showDistance(clickedTileIndex) {
+    let distance = getDistance(clickedTileIndex, winningTile);
     for (let i = 0; i < numberOfTiles * numberOfTiles; i++) {
         let tile = tiles[i];
-        if (i === clickedTile) {
+        if (i === clickedTileIndex) {
             tile.classList.add("clickable");
             let distanceSpan = document.createElement("span");
             distanceSpan.classList.add("distance");
@@ -69,10 +84,10 @@ function hideGameOverScreen() {
 }
 
 // Calculate the Manhattan distance between two tiles
-function getDistance(tile1, tile2) {
-    let x1 = tile1 % numberOfTiles;
-    let y1 = Math.floor(tile1 / numberOfTiles);
-    let x2 = tile2 % numberOfTiles;
-    let y2 = Math.floor(tile2 / numberOfTiles);
+function getDistance(tileIndex1, tileIndex2) {
+    let x1 = tileIndex1 % numberOfTiles;
+    let y1 = Math.floor(tileIndex1 / numberOfTiles);
+    let x2 = tileIndex2 % numberOfTiles;
+    let y2 = Math.floor(tileIndex2 / numberOfTiles);
     return Math.abs(x2 - x1) + Math.abs(y2 - y1);
 }
